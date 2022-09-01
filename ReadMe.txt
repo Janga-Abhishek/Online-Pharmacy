45626a96aa35bca484be0a307c47cd19c6b1f700
Janga-Abhishek <28713574+Janga-Abhishek@users.noreply.github.com>
2022-09-01 15:42:00 +0100

    Add files via upload

---
+++ ReadMe.txt
@@ -0,0 +1,11 @@
+Install and start XAMPP server.
+
+Start all the servers live MySQL, Apache.
+
+Then inside XAMPP folder in htdocs create a new folder and paste the complete code in that directory.
+
+Then go to the webpage and type the local host URL such as "http://localhost:8080/" followed by the file directory where the index.php is located.
+
+Then go to phpMyAdmin page and then create a database named "project" and copy the entire from projet.sql file and paste it in the SQL tab of the selected database and run all the queries.
+
+Then you can perform all the operations in the application.
---
+++ index.php
@@ -0,0 +1,139 @@
+<?php include 'includes/header.php';?>
+
+<div class="image">
+                    <div class="container">
+                        <p class="banner_text">Get your tablets ready.......<br>even before you get ready.
+                        </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="container our_services">
+                <h1>our services</h1>
+            <div class="main1 quick-links">
+                <div class="container">
+                    <div class="row" style="display:flex; margin-left: 0px;margin-right: -15px;">
+                    <bottombox>
+                        <img class="service_images" src="icon_images/available.png" alt="available"><br>
+                        <p5>24/7 Service</p5>
+                    </bottombox>
+                    <bottombox style="background-color: #F9B900;">
+                        <img class="service_images" src="icon_images/fast_delivery.png" alt="fast_delivery" ><br>
+                        <p5>Home delivery</p5>
+                    </bottombox>
+                    <bottombox>
+                        <img class="service_images" src="icon_images/instore.png" alt="instore pickup"><br>
+                        <p5>Instore pick-up</p5>
+                    </bottombox>
+                </div>    
+                </div>
+                </div>
+                <hr>
+                <div class="row">
+
+                    <div class="col-sm-6 col-md-push-6">
+                        <div class="image1" style="text-align: center;"></div>
+                    </div>
+                    <div class="col-sm-6 col-md-pull-6 " style="margin-top: 60px;">
+                    <br>       
+                    <br><p5>24/7 Service</p5><br>
+                    <p4>____________</p4>
+                    <br><br><p6>
+                    We provide 24/7 service. Since it is online you can upload you prescription and place your order any time.</p6>
+                    </div>
+                  
+                </div>
+            </div><br><br>
+            <div class="container">
+                <hr>
+                <div class="row">
+                    <div class="col-sm-6">
+                        <div class="image2" style="text-align: center;"></div>
+                    </div>
+
+                    <div class="col-sm-6" style="margin-top: 60px; padding-left: 50px;">
+                        <br><p5>Home Delivery</p5><br>
+                        <p4>____________</p4>
+                        <br><br><p6>
+                        We offer home delivery, you can sit at home and upload your prescription, the rest is taken care by us. We assure you the fast and online delivery service.
+                        </p6>                    </div>
+                </div>
+            </div><br><br>
+            <div class="container">
+                <hr>
+                <div class="row">
+                    <div class="col-sm-6  col-md-push-6">
+                        <div class="image3" style="text-align: center;"></div>
+                    </div>
+                    <div class="col-sm-6  col-md-pull-6 " style="margin-top: 60px;">
+                        <br><p5>Instore pick-up</p5><br>
+                        <p4>____________</p4>
+                        <br><br><p6>
+                            You can upload your prescription and just need to wait for your order to get ready. Once the order is ready we will notify you to pick-up your order.
+                        </p6>
+                    </div>    
+                    
+                </div><hr class="divider">
+            </div><br><br>
+           
+            
+            <div class="container"   style="width:30%; line-height: 1.6;   border-style: solid;
+            border-color:black; ;
+            border-radius: 90px;
+            text-align: center;
+            padding: 20px; 
+            height: auto;">
+                    
+                            <p7>Join us by clicking on the sign up button</p7><br>
+                            <a class="btn btn-first" href="#">Sign up</a>
+            </div>  <br><br>
+           
+            <br>  
+           
+            <div class="bottom-header1">
+                <div class="main1">
+                    <div class="container">
+                        <div class="row" style="display: flex;">
+                        <div class="col-sm-6" style="
+                        align-items: center;
+                       background-color: whitesmoke;
+                       border-radius: 15px;
+                       padding-left: 20%;
+                       margin-right: -3%;
+                       height: auto;">
+                     <p9>
+                    Connect with us
+                    </p9><br>
+                    <ul>
+                        <li class="linkedIn"><a href=""></a></li>
+                        <li class="facebook"><a href=""></a></li>
+                        <li class="twitter"><a href=""></a></li>
+                    </ul>
+                    </div>                        
+                        <div class="col-sm-6"style=" text-align: left;
+                        line-height: 1.6;
+                        background-color: whitesmoke;
+                        border-radius: 15px;
+                        padding-left: 93px;
+                        margin-left: 6%;
+                        height: auto;">
+                            <p9>
+                            Contact Us
+                            </p9><br>
+                            <ul>
+                                <li class="mail" style="margin-left: -46px;"><a href="#" style="margin-right:10px"></a><div><p13>info@onlinepharmacyservice.com</p13></div></li>
+                                
+                            </ul>
+                        </div>
+                    </div>
+                </div>    
+    
+                </div>
+            </div>
+            <div class="footer">
+                    <div class="container">
+               
+            </div>
+               </div>
+</body>
+</html>
