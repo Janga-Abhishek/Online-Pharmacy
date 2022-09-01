9afd85f97066401ab338fcd28a161a9ee516536b
Janga-Abhishek <28713574+Janga-Abhishek@users.noreply.github.com>
2022-09-01 15:27:17 +0100

    Add files via upload

---
+++ includes/header.php
@@ -0,0 +1,59 @@
+<!Doctype html>
+<html>
+    <head>
+        <title>Online Pharmacy</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+
+
+    </head>
+    <body>
+        <header class="header">
+            <div class="top-header">
+                <div class="container">
+                    <ul>
+                        <li class="linkedIn"><a href=""></a></li>
+                        <li class="facebook"><a href=""></a></li>
+                        <li class="twitter"><a href=""></a></li>
+                    </ul>
+                </div>
+            </div>
+            
+            <nav class="navbar navbar-style">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#micon">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a href="index.php"><img class="logo" src="/MScProject/code/Images/logo.png"><img class="logo" src="/MScProject/code/Images/logo_title.png"></a>
+                </div>
+                <div class="collapse navbar-collapse" id="micon">
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#">about us</a></li>
+                    <li class="dropdown">
+                        <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                          services <span class="caret"></span></a>
+                          <ul class="dropdown-menu" role="menu">
+                            <li><a href="#">service 1</a></li>
+                            <li><a href="#">service 2</a></li>
+                            <li><a href="#">service 3</a></li>                        
+                          </ul>
+                        </li>
+                    <li> 
+                        <a href="./user_authentication/userLogin.php"><img class="login_user" src="/MScProject/code/Images/login_user.png"><p class="loginbutton">login</p></a>
+                    </li>
+                       
+                                     </ul>
+                </div>
+            </div>    
+            </nav>        </header>
